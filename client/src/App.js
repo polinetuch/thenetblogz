94870eccd17d94f3eb64f27429f6bbc5154ee0e5
Polinet <uchpolinet@gmail.com>
2020-04-28 16:45:17 +1000

    adding navbar to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <h1>The Net Blogz</h1>
+          <div className="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="col-sm-8">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              </div>
+            </nav>
+          </div>
+        </div>
       </header>
+  {/* <a class="navbar-brand" href="#">Navbar</a> */}
+  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> */}
+    {/* <span class="navbar-toggler-icon">Button</span>
+  </button> */}
+  {/* <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">About Me</a>
+      <a class="nav-item nav-link" href="#">Blog</a>
+      <a class="nav-item nav-link" href="#">Github</a>
+    </div>
+  </div> */}
+      
     </div>
   );
 }
