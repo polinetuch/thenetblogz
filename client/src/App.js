25a6812ca5c1ad84deeb3d7796bd2a494cf76b6d
Polinet <uchpolinet@gmail.com>
2020-04-29 13:50:12 +1000

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,19 @@ function App() {
           <h1>The Net Blogz</h1>
           <div className="row">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="col-sm-8">
+              <div className="col-sm-4">
                 <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              </div>
+              <div className="col-sm-4">
+                <a className="nav-item nav-link active" href="#">About Me</a>
+              </div>
+              <div className="col-sm-4">
+                <a className="nav-item nav-link active" href="#">Blog</a>
               </div>
             </nav>
           </div>
         </div>
       </header>
-  {/* <a class="navbar-brand" href="#">Navbar</a> */}
-  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> */}
-    {/* <span class="navbar-toggler-icon">Button</span>
-  </button> */}
-  {/* <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">About Me</a>
-      <a class="nav-item nav-link" href="#">Blog</a>
-      <a class="nav-item nav-link" href="#">Github</a>
-    </div>
-  </div> */}
       
     </div>
   );
