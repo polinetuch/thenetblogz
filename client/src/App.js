fda003d4623ccf59f7df9f509a52424bc58f3473
Polinet <uchpolinet@gmail.com>
2020-04-29 16:43:22 +1000

    moving navbar to components

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,14 @@ function App() {
       <header className="App-header">
         <div className="container">
           <h1>The Net Blogz</h1>
-          <div className="row">
+          <Router>
+            <div>
+              
+            </div>
+          </Router>
+          {/* <div className="row">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="col-sm-4">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-              </div>
+              
               <div className="col-sm-4">
                 <a className="nav-item nav-link active" href="#">About Me</a>
               </div>
@@ -19,7 +22,7 @@ function App() {
                 <a className="nav-item nav-link active" href="#">Blog</a>
               </div>
             </nav>
-          </div>
+          </div> */}
         </div>
       </header>
       
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About() {
+    <>
+    <div className="col-sm-4">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+    </div>
+    </>
+}
