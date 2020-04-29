6b55dc987efb42a59b695211b277f184cc731657
Polinet <uchpolinet@gmail.com>
2020-04-29 16:33:52 +1000

    api routes for get and post

--- server/config/schema.sql
+++ server/config/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE 'blogs' (
     `id` Int(11) AUTO_INCREMENT NOT NULL,
     `title` VARCHAR(255) NOT NULL,
     `body` VARCHAR(800) NOT NULL,
-    `created at` DATETIME NOT NULL,
+    `created_at` DATETIME NOT NULL,
 
     PRIMARY KEY (`id`)
 )
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../config/db-connection');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  connection.query('SELECT * from thenetblogz', function(error, results, fields) {
-    if (error) throw error;
-    res.send(JSON.stringify(results));
+/* GET all blogs. */
+router.get('/api/all', function(req, res, next) {
+  var dbQuery = "SELECT * FROM blogs";
+
+  connection.query(dbQuery, function(err, result) {
+    if (err) throw err;
+    res.json(result);
   })
+});
+
+router.get('/api/new', function(req, res) {
+  console.log("New blog entry: ");
+  console.log(req.body);
+
+  var dbQuery = "INSERT INTO blogs (title, body, create_at) VALUES(?,?,?)";
+
+  connection.query(dbQuery, [req.body.title, req.body.body, req.body.create_at],
+    function(err, result) {
+      if (err) throw err;
+      console.log("Successfully uploaded blog");
+      res.send();
+    })
 });
 
 module.exports = router;
