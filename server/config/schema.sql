8c568f043f3620121722aa1d631364bbea9d371e
Polinet <uchpolinet@gmail.com>
2020-04-29 15:52:25 +1000

    created table and columns for db

--- server/config/schema.sql
+++ server/config/schema.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS thenetblogz;
 
 CREATE DATABASE thenetblogz;
+USE thenetblogz
+
+CREATE TABLE 'blogs' (
+    `id` Int(11) AUTO_INCREMENT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `body` VARCHAR(800) NOT NULL,
+    `created at` DATETIME NOT NULL,
+
+    PRIMARY KEY (`id`)
+)
